8242f16c09bca6a3f36ad2b94e7ec60afa3b3e13
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2024-01-17 11:20:23 +0800

    bunnierabbet

--- Gdgrap1Sample.cpp
+++ Gdgrap1Sample.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 #include "DrawPentagon.h"
 
+#define TINYOBJLOADER_IMPLEMENTATION 
+#include "tiny_obj_loader.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -24,6 +27,32 @@ int main(void)
     /*Put code below here*/
     
     gladLoadGL();
+    
+    std::string path = "3D/bunny.obj";
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> material;
+    std::string warning, error;
+
+    tinyobj::attrib_t attributes;
+
+    bool success = tinyobj::LoadObj(
+        &attributes,
+        &shapes,
+        &material,
+        &warning,
+        &error,
+        path.c_str()
+    );
+
+    //get the EBO indices array
+    std::vector<GLuint> mesh_indices;
+    for (int i = 0; i < shapes[0].mesh.indices.size(); i++) {
+        mesh_indices.push_back(
+            shapes[0].mesh.indices[i].vertex_index
+        );
+    }
+
+
 
     GLfloat vertices[]{
         // x,    y,    z 
@@ -43,11 +72,15 @@ int main(void)
 
     glBindVertexArray(VAO); // assigns VAO currently being edited
     glBindBuffer(GL_ARRAY_BUFFER, VBO); // assigns VBO currently being edited and attaches VBO to VAO
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // sizeof(vertices) -> size of array in bytes, GL_STATIC_DRAW as the model doesn't move
+    
+    glBufferData(GL_ARRAY_BUFFER, 
+        sizeof(GL_FLOAT) * attributes.vertices.size(), //size in bytes
+        attributes.vertices.data(), //array
+        GL_STATIC_DRAW); // GL_STATIC_DRAW as the model doesn't move
 
     glVertexAttribPointer(
         0,
-        3,
+        3, //x,y,z
         GL_FLOAT,
         GL_FALSE,
         3 * sizeof(float),
@@ -57,10 +90,11 @@ int main(void)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
     glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-        sizeof(indices),
-        indices,
+        sizeof(GLuint) * mesh_indices.size(),
+        mesh_indices.data(),
         GL_STATIC_DRAW
     );
+
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -76,8 +110,8 @@ int main(void)
         /*put rendering stuff here*/
         glBindVertexArray(VAO);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
-        //DrawPentagon::draw(0.5f);
+        glDrawElements(GL_TRIANGLES, mesh_indices.size(), GL_UNSIGNED_INT, 0);
+        //DrawPentagon::draw(0.5f)
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
