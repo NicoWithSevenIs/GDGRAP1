73108cad3ad2be888ba632186d42135f5fa5aeb4
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2024-01-17 10:16:37 +0800

    triangle

--- Gdgrap1Sample.cpp
+++ Gdgrap1Sample.cpp
@@ -22,7 +22,31 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     /*Put code below here*/
- 
+    
+    gladLoadGL();
+
+    GLfloat vertices[]{
+        // x,    y,    z 
+          0.f, 0.5f, 0.f,
+          -0.5f, -0.5f, 0.f,
+          0.5f,   -0.5f, 0.f
+    };
+
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO); // line responsible for VAO
+    glGenBuffers(1, &VBO); // line responsible for VBO
+
+    glBindVertexArray(VAO); // assigns VAO currently being edited
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // assigns VBO currently being edited and attaches VBO to VAO
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // sizeof(vertices) -> size of array in bytes, GL_STATIC_DRAW as the model doesn't move
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -31,6 +55,8 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         /*put rendering stuff here*/
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         //DrawPentagon::draw(0.5f);
         /* Swap front and back buffers */
@@ -42,6 +68,8 @@ int main(void)
     }
 
     /*Cleanup here*/
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
 }
