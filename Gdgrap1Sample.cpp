efa8d8fcb0eab44c8428b6f1dee2c7d2fcc3d9db
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2024-01-17 10:36:48 +0800

    backup

--- Gdgrap1Sample.cpp
+++ Gdgrap1Sample.cpp
@@ -27,27 +27,46 @@ int main(void)
 
     GLfloat vertices[]{
         // x,    y,    z 
-          0.f, 0.5f, 0.f,
-          -0.5f, -0.5f, 0.f,
-          0.5f,   -0.5f, 0.f
+          0.f, 0.5f, 0.f, //0
+          -0.5f, -0.5f, 0.f, //1
+          0.5f,   -0.5f, 0.f //2
+    };
+    
+    GLuint indices[]{
+        0, 1, 2
     };
 
-    GLuint VAO, VBO;
+    GLuint VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO); // line responsible for VAO
     glGenBuffers(1, &VBO); // line responsible for VBO
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO); // assigns VAO currently being edited
     glBindBuffer(GL_ARRAY_BUFFER, VBO); // assigns VBO currently being edited and attaches VBO to VAO
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // sizeof(vertices) -> size of array in bytes, GL_STATIC_DRAW as the model doesn't move
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        3 * sizeof(float),
+        (void*)0
+    );
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+        sizeof(indices),
+        indices,
+        GL_STATIC_DRAW
+    );
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -56,8 +75,8 @@ int main(void)
 
         /*put rendering stuff here*/
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
         //DrawPentagon::draw(0.5f);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -70,6 +89,7 @@ int main(void)
     /*Cleanup here*/
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
     return 0;
 }
