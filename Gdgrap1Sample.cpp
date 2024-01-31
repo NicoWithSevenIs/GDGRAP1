6f93daa41c97e3c4d8fe72d8cea25e1f2787e6b0
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2024-02-01 02:55:01 +0800

    cleaned up code

--- Gdgrap1Sample.cpp
+++ Gdgrap1Sample.cpp
@@ -5,7 +5,6 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <GLFW/glfw3.h>
-#include "DrawPentagon.h"
 
 #define TINYOBJLOADER_IMPLEMENTATION 
 #include "tiny_obj_loader.h"
@@ -20,19 +19,6 @@ float scale_x = 5, scale_y = 5, scale_z = 1;
 float theta = 90;
 float axis_x = 0, axis_y = 1, axis_z = 0;
 
-
-void Key_Callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (!(action == GLFW_PRESS))
-        return;
-
-    switch (key) {
-        case GLFW_KEY_A: theta-=15; break;
-        case GLFW_KEY_D: theta+=15; break;
-        case GLFW_KEY_W: scale_x += 0.1f; scale_y += 0.1f;  break;
-        case GLFW_KEY_S: scale_x -= 0.1f; scale_y -= 0.1f;  break;
-        
-    }
-}
 
 
 int main(void)
@@ -56,10 +42,8 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     /*Put code below here*/
-    
-    gladLoadGL();
 
-    glfwSetKeyCallback(window, Key_Callback);
+    gladLoadGL();
 
     std::fstream vertSrc("Shaders/shaders.vert");
 
@@ -68,7 +52,7 @@ int main(void)
 
     std::string vertS = vertBuff.str();
     const char* v = vertS.c_str();
-    
+
     std::fstream fragSrc("Shaders/shaders.frag");
     std::stringstream fragBuff;
     fragBuff << fragSrc.rdbuf();
@@ -76,6 +60,7 @@ int main(void)
     std::string fragS = fragBuff.str();
     const char* f = fragS.c_str();
 
+    //Initializing Shaders
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &v, NULL);
     glCompileShader(vertexShader);
@@ -90,6 +75,7 @@ int main(void)
 
     glLinkProgram(shaderProg);
 
+    //Readingfor Mesh
     std::string path = "3D/bunny.obj";
     std::vector<tinyobj::shape_t> shapes;
     std::vector<tinyobj::material_t> material;
@@ -106,12 +92,12 @@ int main(void)
         path.c_str()
     );
 
+    //Preparing Buffer Objects
     //get the EBO indices array
     std::vector<GLuint> mesh_indices;
-    for (int i = 0; i < shapes[0].mesh.indices.size(); i++) {
-        mesh_indices.push_back(
-            shapes[0].mesh.indices[i].vertex_index
-        );
+
+    for (auto i : shapes[0].mesh.indices) {
+        mesh_indices.push_back(i.vertex_index);
     }
 
     GLuint VAO, VBO, EBO;
@@ -119,14 +105,14 @@ int main(void)
     glGenBuffers(1, &VBO); // line responsible for VBO
     glGenBuffers(1, &EBO);
 
-
     glBindVertexArray(VAO); // assigns VAO currently being edited
     glBindBuffer(GL_ARRAY_BUFFER, VBO); // assigns VBO currently being edited and attaches VBO to VAO
     
     glBufferData(GL_ARRAY_BUFFER, 
         sizeof(GL_FLOAT) * attributes.vertices.size(), //size in bytes
         attributes.vertices.data(), //array
-        GL_STATIC_DRAW); // GL_STATIC_DRAW as the model doesn't move
+        GL_STATIC_DRAW
+    ); // GL_STATIC_DRAW as the model doesn't move
 
     glVertexAttribPointer(
         0,
@@ -151,10 +137,6 @@ int main(void)
     glBindVertexArray(0);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    //unsigned int xLoc = glGetUniformLocation(shaderProg, "x");
-    //unsigned int yLoc = glGetUniformLocation(shaderProg, "y");
-
  
   
 
@@ -186,17 +168,11 @@ int main(void)
 
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transformation_matrix));
        
-        //glUniform1f(xLoc, x_mod);
-
-      
-       // glUniform1f(yLoc, y_mod);
-
-        /*put rendering stuff here*/
         glUseProgram(shaderProg);
         glBindVertexArray(VAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
         glDrawElements(GL_TRIANGLES, mesh_indices.size(), GL_UNSIGNED_INT, 0);
-        //DrawPentagon::draw(0.5f)
+
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
@@ -206,6 +182,7 @@ int main(void)
     }
 
     /*Cleanup here*/
+
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
